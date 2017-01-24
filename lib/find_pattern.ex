ed8b53c4ffe3613f0d4c566518119f08dc28328a
Rory Quinlan <rjamesquinlan@gmail.com>
2017-01-24 13:42:35 -0300

    optimized out need to split up input string into all possible matches

--- lib/find_pattern.ex
+++ lib/find_pattern.ex
@@ -1,28 +1,11 @@
 defmodule FindPattern do
 
-  def shortest_repeating_pattern(input_string) do
-    input_string
-    |> ordered_possible_patterns
-    |> Enum.filter(&pattern_matches?(&1, input_string))
-    |> Enum.at(-1)
-  end
-
-  defp ordered_possible_patterns(input_string, patterns \\ [], counter \\ 0) do
-    last_pattern_added = if length(patterns) < 1 do
-      ""
+  def shortest_repeating_pattern(input_string, counter \\ 0) do
+    test_pattern = String.slice(input_string, 0..counter)
+    if pattern_matches?(test_pattern, input_string) do
+      test_pattern
     else
-      [last | _rest] = patterns
-      last
-    end
-
-    if last_pattern_added == input_string do
-      patterns
-    else
-      ordered_possible_patterns(
-        input_string,
-        [String.slice(input_string, 0..counter) | patterns],
-        counter + 1
-      )
+      shortest_repeating_pattern(input_string, counter + 1)
     end
   end
 
@@ -37,7 +20,7 @@ defmodule FindPattern do
     match_section_length = String.length(match_section)
 
     if match_section_length < pattern_length do
-      match_section == String.slice(pattern, 0..(match_section_length - 1))
+      match_section === String.slice(pattern, 0..(match_section_length - 1))
     else
       if match_section === pattern do
         if remainder === "" do
